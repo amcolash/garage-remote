170a5c9121f10ea5dd7bd3e7d43b6a02c2c9c038
Andrew McOlash <amcolash@gmail.com>
2022-07-03 22:03:33 -0700

    add service worker and web manifest

--- src/index.jsx
+++ src/index.jsx
@@ -4,3 +4,9 @@ import { media } from 'typestyle';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
+});
